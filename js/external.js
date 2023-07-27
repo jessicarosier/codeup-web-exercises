c8ac20709836c461a6f347fa58cd1112584af0b4
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-07-27 14:16:41 -0500

    began the lesson about external js

---
+++ js/external.js
@@ -0,0 +1,7 @@
+"use strict";
+
+console.log("Hello from external JavaScript.");
+alert("Welcome to my Website!");
+
+let userFavoriteColor = prompt("What is your favorite color?");
+alert("Great, " + userFavoriteColor + " is my favorite color too!");
