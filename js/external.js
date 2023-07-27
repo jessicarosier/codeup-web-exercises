d30cd20b09761d1b8914726e033dec3f100805e9
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-07-27 15:11:12 -0500

    modified the finished external JavaScript exercise.

--- js/external.js
+++ js/external.js
@@ -12,8 +12,8 @@ let littleMermaidDays = 3,
     brotherBearDays = 5,
     herculesDays = prompt("How many days did you keep Hercules?"),
     pricePerDay = 3,
-    totalRentalPrice = (littleMermaidDays + brotherBearDays + herculesDays) * pricePerDay;
-console.log("You owe " + totalRentalPrice + " for your movie rentals!");
+    totalRentalPrice = (littleMermaidDays * pricePerDay) + ((Number(brotherBearDays)) * pricePerDay) + (herculesDays * pricePerDay);
+alert("You owe $" + totalRentalPrice + " for your movie rentals!");
 
 
 // Suppose you're working as a contractor for 3 companies: Google, Amazon and Facebook, they pay you a different rate per hour. Google pays $400, Amazon $380, and Facebook $350. How much will you receive in payment for this week? You worked 10 hours for Facebook, 6 hours for Google and 4 hours for Amazon.
@@ -25,16 +25,16 @@ let googlePayRate = 400,
     amazonHoursWorked = 4,
     facebookHoursWorked = 10,
     totalWeeklyPay = (googlePayRate * googleHoursWorked) + (amazonPayRate * amazonHoursWorked) + (facebookPayRate * facebookHoursWorked);
-console.log("Your total weekly pay this week is " + totalWeeklyPay);
+alert("Your total weekly pay this week is $" + totalWeeklyPay);
 
 // A student can be enrolled in a class only if the class is not full and the class schedule does not conflict with her current schedule.
 
 let classSize = prompt("How many students are enrolled in the class?"),
-    classNotFull = classSize <= 50,
+    classNotFull = Number(classSize) <= 50,
     noScheduleConflict = confirm("Click OK if you do not have a schedule conflict, otherwise click Cancel."),
     canEnrollInClass = classNotFull && noScheduleConflict;
 
-console.log("It is " + canEnrollInClass + " that you can enroll in this class.");
+alert("It is " + canEnrollInClass + " that you can enroll in this class.");
 
 
 // A product offer can be applied only if a person buys more than 2 items, and the offer has not expired. Premium members do not need to buy a specific amount of products.
@@ -43,8 +43,8 @@ console.log("It is " + canEnrollInClass + " that you can enroll in this class.")
 let itemsPurchased = prompt("How many items did you purchase?"),
     isPremiumMember = confirm("Click OK if your are a premium member, otherwise click Cancel."),
     offerNotExpired = confirm("Click OK is the offer is NOT expired, otherwise click Cancel."),
-    productOfferIsApplied = (itemsPurchased > 2 || isPremiumMember) && offerNotExpired;
-console.log("It is " + productOfferIsApplied + " that the offer can be applied.");
+    productOfferIsApplied = ((Number(itemsPurchased)) > 2 || isPremiumMember) && offerNotExpired;
+alert("It is " + productOfferIsApplied + " that the offer can be applied.");
 
 
 
