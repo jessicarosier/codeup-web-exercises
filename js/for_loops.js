613793bf776427f23b545593ed310c959b5fdc32
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-08-03 16:26:59 -0500

    began loops exercise

--- js/for_loops.js
+++ js/for_loops.js
@@ -1,25 +1,101 @@
-(function(){
+(function() {
     // Create a function named showMultiplicationTable that accepts a number and console.logs the multiplication table for that number (just multiply by the numbers 1 through 10)
-function showMultiplicationTable(number) {
-    console.log(number * 1);
-    console.log(number * 2);
-    console.log(number * 3);
-    console.log(number * 4);
-    console.log(number * 5);
-    console.log(number * 6);
-    console.log(number * 7);
-    console.log(number * 8);
-    console.log(number * 9);
-    console.log(number * 10);
-}
 
-    console.log(showMultiplicationTable(2));
+    // 7 x 1 = 7
+    // 7 x 2 = 14
+    // 7 x 3 = 21
+    // 7 x 4 = 28
+    // 7 x 5 = 35
+    // 7 x 6 = 42
+    // 7 x 7 = 49
+    // 7 x 8 = 56
+    // 7 x 9 = 63
+    // 7 x 10 = 70
 
-    // Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even. For example:
+// function showMultiplicationTable(number) {
+//     for(let x=1; x <= 10; x++) {
+//         console.log(number + " x " + x + " = " + (x * number));
+//     }
+// }
+// showMultiplicationTable(7)
 
-    // for (/*initialization*/; /*condition*/; /*increment*/) {
-    //     // body
-    // }
+
+//2 - Use a for loop and the code from the previous lessons to generate 10 random numbers between 20 and 200 and output to the console whether each number is odd or even.
+//     123 is odd
+//     80 is even
+//     24 is even
+//     199 is odd
+
+
+// for(x = 1; x <=10; x++) {
+//     let random = Math.floor((Math.random() * 200) + 20);
+//     if (random % 2 === 0) {
+//         console.log (random + " is even.");
+//     } else {
+//         console.log (random + " is odd.");
+//     }
+// }
+
+
+
+//TODO: come back to this one later....brain hurts
+//4 - Create a for loop that uses console.log to create the output shown below.
+//     1
+//     22
+//     333
+//     4444
+//     55555
+//     666666
+//     7777777
+//     88888888
+//     999999999
+
+// for(x = 1; x <= 9; x++) {
+//
+//     for(y = 1; y <= 9; y++) {
+//         if (y == 1) {
+//             continue;
+//         }
+//     }
+//     console.log(x,y)
+//     }
+
+
+
+
+//5 - Create a for loop that uses console.log to create the output shown below.
+//     100
+//     95
+//     90
+//     85
+//     80
+//     75
+//     70
+//     65
+//     60
+//     55
+//     50
+//     45
+//     40
+//     35
+//     30
+//     25
+//     20
+//     15
+//     10
+//     5
+
+// for (let x = 100; x >= 5; x -=5 ) {
+//   console.log(x)
+// }
+
+
+
+
+
+
+
+
 
 
 })();
