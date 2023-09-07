7e68119fbce2caaebd0dbac6ed6a1ce1742a5686
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-09-07 14:29:11 -0500

    refactored konami js and added styling

--- js/jquery.js
+++ js/jquery.js
@@ -1,26 +1,27 @@
 "use strict";
-
 let textDisplay = $("#text-appear");
+let refreshButton = $("#reset");
+let hintButton = $("#get-hint");
 let secretCode = "";
 let audio = "";
-let html = "Begin typing...";
+let html = "";
 
 let passcodes = [
   {
     key: "cat",
-    passcode: "77697987",
+    passcode: "676584",
     img: "img/konami/cat.jpg",
     audio: "audio/meow.mp3",
   },
   {
     key: "dog",
-    passcode: "66658275",
+    passcode: "687971",
     img: "img/konami/dog.jpg",
     audio: "audio/bark.mp3",
   },
   {
     key: "bird",
-    passcode: "6772738280",
+    passcode: "66738268",
     img: "img/konami/bird.jpg",
     audio: "audio/chirp.mp3",
   },
@@ -30,32 +31,63 @@ let passcodes = [
     img: "img/konami/konami-code.jpeg",
     audio: "audio/konami.mp3",
   },
+  {
+    key: "pokemon",
+    passcode: "80797569777978",
+    img: "img/konami/pikachu.jpg",
+    audio: "audio/pikachu.mp3",
+  },
+  {
+    key: "spongebob",
+    passcode: "838079787169667966",
+    img: "img/konami/spongebob.jpg",
+    audio: "audio/pikachu.mp3",
+  },
 ];
 
-textDisplay.html(html);
+function displayPasscode(passcode) {
+  let html = "";
+  html += `<p class="fs-3">You cracked the <span class="text-uppercase text-danger">${passcode.key}</span> code!</p>`;
+  html += `<img src=${passcode.img} class="h-auto w-50""/>`;
 
-function renderPasscode(passcode) {
-  let html =
-    '<div class="d-flex flex-column justify-content-center align-items-center">';
-  html += `<p>You cracked the ${passcode.key}</p>`;
-  html += `<img src=${passcode.img} class="h-auto w-25""/>`;
-  html += `</div>`;
   return html;
 }
 
 $("body").on(`keyup`, (event) => {
+  event.preventDefault();
+  $("#konami-header").css("color", "red");
   let code = event.keyCode;
   secretCode += code;
   textDisplay.html(secretCode);
+  console.log(secretCode);
 
   for (let i = 0; i < passcodes.length; i++) {
-    if (passcodes[i].passcode == secretCode) {
+    // textDisplay.html(secretCode);
+    if (passcodes[i].passcode === secretCode) {
       console.log(passcodes[i].passcode);
       html = "";
-      html += renderPasscode(passcodes[i]);
+      html += displayPasscode(passcodes[i]);
       audio = new Audio(passcodes[i].audio);
+      console.log(audio);
+      textDisplay.html(html);
+      audio.play();
     }
   }
-  textDisplay.html(html);
-  audio.play();
+});
+
+refreshButton.on(`click`, (event) => {
+  location.reload();
+});
+
+hintButton.on(`click`, (event) => {
+  let hints = [
+    "Who lives in a pineapple under the sea?",
+    "Gotta catch em all!",
+    "meow",
+    "woof!",
+    "chirp chirp",
+    "NES 1986",
+  ];
+  let hintHtml = `<p>${hints[Math.floor(Math.random() * hints.length)]}</p>`;
+  textDisplay.html(hintHtml);
 });
