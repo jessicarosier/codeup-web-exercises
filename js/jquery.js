c309b5580590ec1f3647151751408a9de7da0061
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-09-07 13:04:20 -0500

    refactored kanomi js to store codes in object

--- js/jquery.js
+++ js/jquery.js
@@ -2,60 +2,60 @@
 
 let textDisplay = $("#text-appear");
 let secretCode = "";
-let meowAudio = new Audio("audio/meow.mp3");
-let barkAudio = new Audio("audio/bark.mp3");
-let chirpAudio = new Audio("audio/chirp.mp3");
-let konamiAudio = new Audio("audio/konami.mp3");
+let audio = "";
+let html = "Begin typing...";
 
-let catPhrase = "77697987";
-let dogPhrase = "66658275";
-let birdPhrase = "6772738280";
-let konamiCode = "3838404037393739666513";
-let html = "";
+let passcodes = [
+  {
+    key: "cat",
+    passcode: "77697987",
+    img: "img/konami/cat.jpg",
+    audio: "audio/meow.mp3",
+  },
+  {
+    key: "dog",
+    passcode: "66658275",
+    img: "img/konami/dog.jpg",
+    audio: "audio/bark.mp3",
+  },
+  {
+    key: "bird",
+    passcode: "6772738280",
+    img: "img/konami/bird.jpg",
+    audio: "audio/chirp.mp3",
+  },
+  {
+    key: "konami",
+    passcode: "3838404037393739666513",
+    img: "img/konami/konami-code.jpeg",
+    audio: "audio/konami.mp3",
+  },
+];
 
-function renderHtml() {
-  html = `
-<p>"YOU CRACKED THE CODE!"</p>
-<img src="img/konami/bird.jpg" class="h-auto w-25">`;
-  textDisplay.html(html);
+textDisplay.html(html);
+
+function renderPasscode(passcode) {
+  let html =
+    '<div class="d-flex flex-column justify-content-center align-items-center">';
+  html += `<p>You cracked the ${passcode.key}</p>`;
+  html += `<img src=${passcode.img} class="h-auto w-25""/>`;
+  html += `</div>`;
+  return html;
 }
 
-$("body").on("keyup", function (event) {
-  event.preventDefault();
+$("body").on(`keyup`, (event) => {
   let code = event.keyCode;
   secretCode += code;
   textDisplay.html(secretCode);
-  console.log(secretCode);
-  if (secretCode === catPhrase) {
-    html = `
-<p>"YOU CRACKED THE CODE! MEOW"</p>
-<img src="img/konami/cat.jpg" class="h-auto w-25">`;
-    meowAudio.play();
-    textDisplay.html(html);
-  }
 
-  if (secretCode === dogPhrase) {
-    html = `
-<p>"YOU CRACKED THE CODE! BARK"</p>
-<img src="img/konami/dog.jpg" class="h-auto w-25">`;
-    barkAudio.play();
-    textDisplay.html(html);
+  for (let i = 0; i < passcodes.length; i++) {
+    if (passcodes[i].passcode == secretCode) {
+      console.log(passcodes[i].passcode);
+      html = "";
+      html += renderPasscode(passcodes[i]);
+      audio = new Audio(passcodes[i].audio);
+    }
   }
-
-  if (secretCode === birdPhrase) {
-    html = `
-<p>"YOU CRACKED THE CODE! CHIRP"</p>
-<img src="img/konami/bird.jpg" class="h-auto w-25">`;
-    chirpAudio.play();
-    textDisplay.html(html);
-  }
-
-  if (secretCode === konamiCode) {
-    html = `
-<p>"YOU CRACKED THE <span id="konami-span" class="fw-bolder fs-4">KONAMI</span> CODE!"</p>
-<img src="img/konami/konami-code.jpeg" class="h-auto w-50">`;
-    konamiAudio.play();
-    textDisplay.html(html);
-    alert("You have added 30 lives!");
-  }
+  textDisplay.html(html);
+  audio.play();
 });
