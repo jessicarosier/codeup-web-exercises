d46a54e6bdd7acb6d5bf58879142bc74951b854a
Jessica Rosier <jessica.c.rosier1@gmail.com>
2023-09-13 14:30:13 -0500

    basic weathermap js functionality completed

--- js/weather_map.js
+++ js/weather_map.js
@@ -1,113 +1,122 @@
 //Constant variable holding Open Weather Map API URL for API call to current weather conditions
-const BASE_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=${OPEN_WEATHER_APPID}`;
+let BASE_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?&units=imperial&appid=${OPEN_WEATHER_APPID}&q=Converse,TX,USA`;
 
-const MY_MOMS_HOUSE = [33.8596246116055, -117.878436750891];
-
-// BASE_WEATHER_URL + `lat=${MY_MOMS_HOUSE[0]}&lon=${MY_MOMS_HOUSE[1]}&appid=${OPEN_WEATHER_APPID}`;.
-
-$.get(BASE_WEATHER_URL + `&q=Converse,TX,USA`).done(function (data) {
-  console.log(data);
-  console.log(data.weather[0].description);
-  let currentWeatherHtml = ``;
-  currentWeatherHtml += `<div>`;
-  currentWeatherHtml += `<p>${data.name}</p>`;
-  currentWeatherHtml += `<p>Current Temp: <span class="fw-bold">${data.main.temp.toFixed(
-    0,
-  )}°F</p></span>`;
-  let currentWeatherImg = "";
-
-  if (data.weather[0].description.includes("light rain")) {
-    currentWeatherImg = "img/weather/cloud-drizzle.svg";
-  } else if (data.weather[0].description.includes("clouds")) {
-    currentWeatherImg = "img/weather/cloud.svg";
-  } else if (data.weather[0].description.includes("snow")) {
-    currentWeatherImg = "img/weather/cloud-snow.svg";
-  } else if (data.weather[0].description.includes("lightning")) {
-    currentWeatherImg = "img/weather/lightning.svg";
-  } else if (data.weather[0].description.includes("windy")) {
-    currentWeatherImg = "img/weather/wind.svg";
-  } else if (data.weather[0].description.includes("clear")) {
-    currentWeatherImg = "img/weather/brightness-high.svg";
-  } else if (data.weather[0].description.includes("rain")) {
-    currentWeatherImg = "img/weather/cloud-rain.svg";
-  }
-
-  $("#todays-weather").html(currentWeatherHtml);
-  $("#todays-img").html(
-    `<img src=${currentWeatherImg} id="current-weather-img" />`,
-  );
-});
-
-//Constant variable holding Open Weather Map API URL for API call to 5 day forecast
-const FIVE_DAY_URL = `https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=${OPEN_WEATHER_APPID}`;
-
-//Sends an API call to weather map 5 day, loops through data to grab specified weather information for 5 consecutive days places info in a variable holding html. Then displays the html in a div with the  ID of #insert-weather
-$.get(FIVE_DAY_URL + `&zip=78109,US`).done(function (data) {
-  console.log(data);
-  let fiveDayHtml = ``;
-  let weatherImg = "";
-
-  //conditional logic determines weatherImg displayed in card html
-  for (let i = 0; i < data.list.length; i += 8) {
-    if (data.list[i].weather[0].description.includes("light rain")) {
-      weatherImg = "img/weather/cloud-drizzle.svg";
-    } else if (data.list[i].weather[0].description.includes("clouds")) {
-      weatherImg = "img/weather/cloud.svg";
-    } else if (data.list[i].weather[0].description.includes("snow")) {
-      weatherImg = "img/weather/cloud-snow.svg";
-    } else if (data.list[i].weather[0].description.includes("lightning")) {
-      weatherImg = "img/weather/lightning.svg";
-    } else if (data.list[i].weather[0].description.includes("windy")) {
-      weatherImg = "img/weather/wind.svg";
-    } else if (data.list[i].weather[0].description.includes("clear")) {
-      weatherImg = "img/weather/brightness-high.svg";
-    }
-    fiveDayHtml += `<div class="d-flex flex-column justify-content-center align-items-center gap-2 w-25">`;
-    fiveDayHtml += `<p class="bg-light text-center p-1">${data.list[
-      i
-    ].dt_txt.substring(0, data.list[i].dt_txt.indexOf(" "))}</p>`;
-    fiveDayHtml += `<p class="d-flex justify-content-center"> High: ${data.list[
-      i
-    ].main.temp_max.toFixed(0)}°F / Low: ${data.list[i].main.temp_min.toFixed(
-      0,
-    )}°F</p>`;
-    fiveDayHtml += `<img src=${weatherImg} class="weather-icon-img" />`;
-    fiveDayHtml += `<div class="p-3">`;
-    fiveDayHtml += `<p>Description: <span class="fw-bold">${data.list[i].weather[0].description}</span></p>`;
-    fiveDayHtml += `<p>Humidity: <span class="fw-bold">${data.list[i].main.humidity}%</span></p>`;
-    fiveDayHtml += `<p>Real Feel: <span class="fw-bold">${data.list[
-      i
-    ].main.feels_like.toFixed(0)}°F</span></p>`;
-    fiveDayHtml += `<p>Wind <span class="fw-bold">${data.list[
-      i
-    ].wind.speed.toFixed(0)}mph</span></p>`;
-    fiveDayHtml += `<p>Pressure: <span class="fw-bold">${(
-      data.list[i].main.pressure / 33.864
-    ).toFixed(2)} inHg</span></p>`;
-    fiveDayHtml += `</div>`;
-    fiveDayHtml += `</div>`;
-  }
-  $("#insert-weather").html(fiveDayHtml);
-});
-
-// function weatherConditions(obj) {
-//   if (obj.list[i].weather[0].includes("rain")) {
-//     weatherImg = "img/weather/drizzle.svg";
-//   }
-// }
+//variable holding Open Weather Map API URL for API call to 5 day forecast
+let FIVE_DAY_URL = `https://api.openweathermap.org/data/2.5/forecast?&units=imperial&appid=${OPEN_WEATHER_APPID}&zip=78109,US`;
 
 //adds mapbox API map to page, displays map in div with ID of #map
 mapboxgl.accessToken = MAPBOX_API_TOKEN;
 const map = new mapboxgl.Map({
   container: "map", // container ID
   style: "mapbox://styles/mapbox/streets-v12", // style URL
-  center: [-117.8713292, 33.8819333], // starting position [lng, lat]
-  zoom: 13, // starting zoom
+  center: [-98.4842, 37.0119], // starting position [lng, lat] //center of U.S
+  projection: "globe",
+  zoom: 5, // starting zoom
 });
 
-// geocode("San Antonio", API_TOKEN_HERE).then(function(results) {
-//   // do something with results
-// })
+const nav = new mapboxgl.NavigationControl({
+  showCompass: true,
+  showZoom: true,
+});
+map.addControl(nav, "top-left");
+
+let draggableMarker = new mapboxgl.Marker({
+  draggable: true,
+})
+  .setLngLat([-98.4842, 37.0119])
+  .addTo(map);
+
+let markerLngLat = draggableMarker.getLngLat();
+
+//event listener runs on page load to display default weather
+addEventListener("load", function (event) {
+  getCurrentWeather(BASE_WEATHER_URL);
+  getFiveDayForecast(FIVE_DAY_URL);
+});
+
+function getCurrentWeather(url) {
+  $.get(url).done(function (data) {
+    let currentWeatherHtml = ``;
+    currentWeatherHtml += `<div>`;
+    currentWeatherHtml += `<p>${data.name}</p>`;
+    currentWeatherHtml += `<p>Current Temp: <span class="fw-bold">${data.main.temp.toFixed(
+      0,
+    )}°F</p></span>`;
+    let currentWeatherImg = "";
+
+    if (data.weather[0].description.includes("light rain")) {
+      currentWeatherImg = "img/weather/cloud-drizzle.svg";
+    } else if (data.weather[0].description.includes("clouds")) {
+      currentWeatherImg = "img/weather/cloud.svg";
+    } else if (data.weather[0].description.includes("snow")) {
+      currentWeatherImg = "img/weather/cloud-snow.svg";
+    } else if (data.weather[0].description.includes("lightning")) {
+      currentWeatherImg = "img/weather/lightning.svg";
+    } else if (data.weather[0].description.includes("windy")) {
+      currentWeatherImg = "img/weather/wind.svg";
+    } else if (data.weather[0].description.includes("clear")) {
+      currentWeatherImg = "img/weather/brightness-high.svg";
+    } else if (data.weather[0].description.includes("rain")) {
+      currentWeatherImg = "img/weather/cloud-rain.svg";
+    }
+
+    $("#todays-weather").html(currentWeatherHtml);
+    $("#todays-img").html(
+      `<img src=${currentWeatherImg} id="current-weather-img" />`,
+    );
+  });
+}
+
+//Sends an API call to weather map 5 day, loops through data to grab specified weather information for 5 consecutive days places info in a variable holding html. Then displays the html in a div with the  ID of #insert-weather
+function getFiveDayForecast(url) {
+  $.get(url).done(function (data) {
+    let fiveDayHtml = ``;
+    let weatherImg = "";
+
+    //conditional logic determines weatherImg displayed in card html
+    for (let i = 0; i < data.list.length; i += 8) {
+      if (data.list[i].weather[0].description.includes("light rain")) {
+        weatherImg = "img/weather/cloud-drizzle.svg";
+      } else if (data.list[i].weather[0].description.includes("clouds")) {
+        weatherImg = "img/weather/cloud.svg";
+      } else if (data.list[i].weather[0].description.includes("snow")) {
+        weatherImg = "img/weather/cloud-snow.svg";
+      } else if (data.list[i].weather[0].description.includes("lightning")) {
+        weatherImg = "img/weather/lightning.svg";
+      } else if (data.list[i].weather[0].description.includes("windy")) {
+        weatherImg = "img/weather/wind.svg";
+      } else if (data.list[i].weather[0].description.includes("clear")) {
+        weatherImg = "img/weather/brightness-high.svg";
+      }
+      fiveDayHtml += `<div class="d-flex flex-column justify-content-center align-items-center gap-2 w-25">`;
+      fiveDayHtml += `<p class="bg-light text-center p-1">${data.list[
+        i
+      ].dt_txt.substring(0, data.list[i].dt_txt.indexOf(" "))}</p>`;
+      fiveDayHtml += `<p class="d-flex justify-content-center"> High: ${data.list[
+        i
+      ].main.temp_max.toFixed(0)}°F / Low: ${data.list[i].main.temp_min.toFixed(
+        0,
+      )}°F</p>`;
+      fiveDayHtml += `<img src=${weatherImg} class="weather-icon-img" />`;
+      fiveDayHtml += `<div class="p-3">`;
+      fiveDayHtml += `<p>Description: <span class="fw-bold">${data.list[i].weather[0].description}</span></p>`;
+      fiveDayHtml += `<p>Humidity: <span class="fw-bold">${data.list[i].main.humidity}%</span></p>`;
+      fiveDayHtml += `<p>Real Feel: <span class="fw-bold">${data.list[
+        i
+      ].main.feels_like.toFixed(0)}°F</span></p>`;
+      fiveDayHtml += `<p>Wind <span class="fw-bold">${data.list[
+        i
+      ].wind.speed.toFixed(0)}mph</span></p>`;
+      fiveDayHtml += `<p>Pressure: <span class="fw-bold">${(
+        data.list[i].main.pressure / 33.864
+      ).toFixed(2)} inHg</span></p>`;
+      fiveDayHtml += `</div>`;
+      fiveDayHtml += `</div>`;
+    }
+    $("#insert-weather").html(fiveDayHtml);
+  });
+}
+
 //takes in a string value and MapBox API Key, returns array containing [lat, lon]
 function geocode(search, token) {
   let baseUrl = "https://api.mapbox.com";
@@ -130,51 +139,84 @@ function geocode(search, token) {
     });
 }
 
-let ids = 0;
-function placeMarkerAndPopup(info, token, map) {
-  geocode(info.address, token).then(function (coordinates) {
-    let popup = new mapboxgl.Popup().setHTML(info.popupHTML);
-    let marker = new mapboxgl.Marker({
-      color: info.color,
-    })
-      .setLngLat(coordinates)
-      .addTo(map)
-      .setPopup(popup);
-    popup.addTo(map);
-    //sets the id of the newly created marker to the next id #
-    marker._element.id = `marker${ids++}`;
-  });
-}
-
 //targets the input element with the ID of #address-search-bar
 const searchButton = document.getElementById("weather-search-button");
 //targets the form element with the ID of #address-search-form
 let searchForm = document.getElementById("search-form");
 //search address form event listener
-
 searchForm.addEventListener(`submit`, function (event) {
   event.preventDefault();
   //grabs the value of all the form inputs
   let formData = new FormData(searchForm, searchButton);
-  con;
   //converts the formData to an object
   let formObj = Object.fromEntries(formData);
-  console.log(formObj);
   //stores the value of the object address property. This will be the string address the user typed in.
   let searchStr = formObj.address;
 
   geocode(searchStr, MAPBOX_API_TOKEN).then(function (results) {
     // do something with results
-    let newMarker = "";
-    newMarker = new mapboxgl.Marker().setLngLat(results).addTo(map);
-    map.setZoom(15);
+    // let newMarker = "";
+    // newMarker = new mapboxgl.Marker().setLngLat(results).addTo(map);
+    draggableMarker.setLngLat(results);
+    map.setZoom(10);
     map.setCenter(results);
+    // console.log(results[0]);
+    // console.log(results[1]);
+    FIVE_DAY_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${results[1]}&lon=${results[0]}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
+    BASE_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${results[1]}&lon=${results[0]}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
+
+    getCurrentWeather(BASE_WEATHER_URL);
+    getFiveDayForecast(FIVE_DAY_URL);
+
+    markerLngLat = draggableMarker.getLngLat();
   });
 });
 
-//targets the markers and popups by class name
-//toggles (adds and removes) the "invisible" class when button is clicked
-$("#hide-markers").on("click", function () {
-  $(".mapboxgl-marker").toggleClass("invisible");
-  $(".mapboxgl-popup").toggleClass("invisible");
-});
+function reverseGeocode(coordinates, token) {
+  var baseUrl = "https://api.mapbox.com";
+  var endPoint = "/geocoding/v5/mapbox.places/";
+  return (
+    fetch(
+      baseUrl +
+        endPoint +
+        coordinates.lng +
+        "," +
+        coordinates.lat +
+        ".json" +
+        "?" +
+        "access_token=" +
+        token,
+    )
+      .then(function (res) {
+        return res.json();
+      })
+      // to get all the data from the request, comment out the following three lines...
+      .then(function (data) {
+        return data.features[0].place_name;
+      })
+  );
+}
+
+//set default display when search occurs, change get request URL
+function updateMarkerLocation() {
+  markerLngLat = draggableMarker.getLngLat();
+  BASE_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${markerLngLat.lat}&lon=${markerLngLat.lng}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
+  FIVE_DAY_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${markerLngLat.lat}&lon=${markerLngLat.lng}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
+  map.setZoom(10);
+  map.setCenter(markerLngLat);
+  getCurrentWeather(BASE_WEATHER_URL);
+  getFiveDayForecast(FIVE_DAY_URL);
+}
+
+// Event listener that fires on marker drag end.
+function onDragEnd() {
+  markerLngLat = draggableMarker.getLngLat();
+  BASE_WEATHER_URL = `https://api.openweathermap.org/data/2.5/weather?lat=${markerLngLat.lat}&lon=${markerLngLat.lng}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
+  FIVE_DAY_URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${markerLngLat.lat}&lon=${markerLngLat.lng}&units=imperial&appid=${OPEN_WEATHER_APPID}`;
+  map.setZoom(10);
+  map.setCenter(markerLngLat);
+  getCurrentWeather(BASE_WEATHER_URL);
+  getFiveDayForecast(FIVE_DAY_URL);
+}
+
+draggableMarker.on("dragend", onDragEnd);
